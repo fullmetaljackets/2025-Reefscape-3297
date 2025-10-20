15ac77569a1df9bd5a57f4bf90845b82466a2891
fullmetaljackets <rkostoh@gmail.com>
2025-10-20 16:59:53 -0500

    setpoint adjustments

--- src/main/java/frc/robot/commands/grouped/BackFloorIntake.java
+++ src/main/java/frc/robot/commands/grouped/BackFloorIntake.java
@@ -23,7 +23,7 @@ public class BackFloorIntake extends SequentialCommandGroup{
             new ArmExtendToSetpoint(-0.7,5, s_ArmExtend),
             new WristRotToSetpoint(0,0.01, s_WristRot),
             // new ArmRotToSetpoint(-0.09,0.01, s_ArmRot),
-            new ArmRotToSetpoint(-0.115,0.05, s_ArmRot),
+            new ArmRotToSetpoint(-0.105,0.05, s_ArmRot),
             // new ArmExtendToSetpoint(1.8,3, s_ArmExtend),
             new ArmExtendToSetpoint(0.95,3, s_ArmExtend),
             // new WristRotToSetpoint(0.15,0.01, s_WristRot)
--- src/main/java/frc/robot/commands/grouped/ReefLV4Over.java
+++ src/main/java/frc/robot/commands/grouped/ReefLV4Over.java
@@ -19,7 +19,7 @@ public class ReefLV4Over extends SequentialCommandGroup{
             new IntakeClose(s_IntakeJaws),
             new ArmExtendToSetpoint(-0.7,2,  s_ArmExtend),
             new WristRotToSetpoint(0,0.01, s_WristRot),
-            new ArmRotToSetpoint(-0.53,0.1, s_ArmRot),
+            new ArmRotToSetpoint(-0.52,0.1, s_ArmRot),
             new ArmExtendToSetpoint(5,1, s_ArmExtend),
             new WristRotToSetpoint(0.1, 0.01, s_WristRot)
         );
